5f3bf388a415764dcbb1a943b268e87f2347d8d3
Hans Garon <hgaron@ets.org>
2022-03-22 08:56:33 -0400

    Added rendering of array entries to UL elements to give visual feedback.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import { useReducer } from 'react';
-import './App.css';
-import { SBT } from './SBT';
-import { produce, Draft, castDraft } from 'immer';
+import { useReducer } from "react";
+import "./App.css";
+import { SBT } from "./SBT";
+import { produce, Draft, castDraft } from "immer";
 
 enum ActionTypes {
-  UpdateSBTState
+  UpdateSBTState,
 }
 
 type UpdateSBTStateAction = {
-  type: ActionTypes.UpdateSBTState,
-  payload: IADCScopedSBTState
-}
+  type: ActionTypes.UpdateSBTState;
+  payload: IADCScopedSBTState;
+};
 
-function createUpdateSBTStateAction(payload: IADCScopedSBTState): UpdateSBTStateAction {
+function createUpdateSBTStateAction(
+  payload: IADCScopedSBTState
+): UpdateSBTStateAction {
   return {
     type: ActionTypes.UpdateSBTState,
-    payload
-  }
+    payload,
+  };
 }
 
 type Actions = UpdateSBTStateAction;
 
 export interface IADCScopedSBTState {
-  [k: string]: any
+  [k: string]: any;
 }
 
 // honestly, we don't even use the state here XD
@@ -33,25 +35,27 @@ function reducer(_state: IADCScopedSBTState, action: Actions) {
   }
 }
 
-const initialState = {}
+const initialState = {};
 
-export type updateStateFunction<T> = (recipe: (draft: Draft<T>) => void) => void;
+export type updateStateFunction<T> = (
+  recipe: (draft: Draft<T>) => void
+) => void;
 // export type updateStateFunction<T> = (fn: (draft: Draft<T>) => WriteableDraft<T>) => void;
-
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  function updateSBTState<IADCScopedSBTState>(recipe: updateStateFunction<IADCScopedSBTState>) {
-    const newState = produce(state, recipe)
+  function updateSBTState<IADCScopedSBTState>(
+    recipe: updateStateFunction<IADCScopedSBTState>
+  ) {
+    const newState = produce(state, recipe);
     debugger;
-    dispatch(createUpdateSBTStateAction(newState))
+    dispatch(createUpdateSBTStateAction(newState));
   }
-
 
   return (
     // maybe we have to do an `any` cast in the ADC side
-    <SBT updateState={updateSBTState as any} />
+    <SBT updateState={updateSBTState as any} state={state as any} />
   );
 }
 
--- src/SBT.tsx
+++ src/SBT.tsx
@@ -1,53 +1,66 @@
-import { useEffect } from "react"
-import { IADCScopedSBTState, updateStateFunction } from "./App"
+import { useEffect } from "react";
+import { IADCScopedSBTState, updateStateFunction } from "./App";
 
 interface ISBTState extends IADCScopedSBTState {
-    sceneOne: {
-        textInputOne: string,
-        arrayOne: number[],
-        textInputTwo: string
-    },
-    sceneTwo: {
-        textInputOne: string,
-        isSomeRandomDrawerOpen: boolean,
-        someComplexObject: {
-            [k: string]: boolean
-        }
-    }
+  sceneOne: {
+    textInputOne: string;
+    arrayOne: number[];
+    textInputTwo: string;
+  };
+  sceneTwo: {
+    textInputOne: string;
+    isSomeRandomDrawerOpen: boolean;
+    someComplexObject: {
+      [k: string]: boolean;
+    };
+  };
 }
 
 interface SBTProps {
-    updateState: updateStateFunction<ISBTState>
+  updateState: updateStateFunction<ISBTState>;
+  state: ISBTState;
 }
 
 const initialState: ISBTState = {
-    sceneOne: {
-        textInputOne: "",
-        arrayOne: [],
-        textInputTwo: ""
-    },
-    sceneTwo: {
-        textInputOne: "",
-        isSomeRandomDrawerOpen: false,
-        someComplexObject: {}
-    }
-}
+  sceneOne: {
+    textInputOne: "",
+    arrayOne: [],
+    textInputTwo: "",
+  },
+  sceneTwo: {
+    textInputOne: "",
+    isSomeRandomDrawerOpen: false,
+    someComplexObject: {},
+  },
+};
 
-export function SBT({ updateState }: SBTProps) {
-    // initialize our state when we first load
-    useEffect(() => {
-        updateState(_ => {
-            return initialState;
-        })
-    }, [])
+export function SBT({ updateState, state }: SBTProps) {
+  // initialize our state when we first load
+  useEffect(() => {
+    updateState((_) => {
+      return initialState;
+    });
+  }, []);
 
-    function handleButtonClick() {
-        updateState(draft => {
-            // typescript baby!
-            draft.sceneOne.arrayOne.push(5);
-        })
-    }
+  function handleButtonClick() {
+    updateState((draft) => {
+      // typescript baby!
+      draft.sceneOne.arrayOne.push(5);
+    });
+  }
 
-
-    return <div>Cool I'm an SBT. <button onClick={handleButtonClick}>Click this button</button>to push to the array in state</div>
+  return (
+    <>
+      <div>
+        Cool I'm an SBT.{" "}
+        <button onClick={handleButtonClick}>Click this button</button>to push to
+        the array in state
+      </div>
+      <ul>
+        {state?.sceneOne?.arrayOne?.map((entry, index) => (
+          <li key={index}>{entry}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
