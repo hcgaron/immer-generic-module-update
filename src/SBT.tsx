adc1048bf3ab9426dbe9a66f39300e9696ee8616
Hans Garon <hgaron@ets.org>
2022-03-22 09:11:28 -0400

    Added some styling and a text input.

--- src/SBT.tsx
+++ src/SBT.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { ChangeEvent, FormEvent, useEffect } from "react";
 import { IADCScopedSBTState, updateStateFunction } from "./App";
 
 interface ISBTState extends IADCScopedSBTState {
@@ -49,18 +49,42 @@ export function SBT({ updateState, state }: SBTProps) {
     });
   }
 
+  function handleTextInputChange(e: ChangeEvent<HTMLInputElement>) {
+    updateState((draft) => {
+      draft.sceneOne.textInputOne = e.target.value;
+    });
+  }
+
   return (
-    <>
-      <div>
+    <div style={{ padding: "1rem" }}>
+      <div style={{ marginBottom: "1rem" }}>
         Cool I'm an SBT.{" "}
-        <button onClick={handleButtonClick}>Click this button</button>to push to
-        the array in state
+        <button
+          onClick={handleButtonClick}
+          style={{ padding: "1rem", fontSize: "24px", marginRight: ".5rem" }}
+        >
+          Click this button
+        </button>
+        to push to the array in state.
       </div>
-      <ul>
-        {state?.sceneOne?.arrayOne?.map((entry, index) => (
-          <li key={index}>{entry}</li>
-        ))}
-      </ul>
-    </>
+      <div>
+        <input
+          type="text"
+          name="text-input-one"
+          id="text-input-one"
+          value={state?.sceneOne?.textInputOne || ""}
+          onChange={handleTextInputChange}
+          style={{ padding: "1rem", marginBottom: "1rem", fontSize: "24px" }}
+        />
+      </div>
+      <div>
+        <span>Below you will see any of the entries in our state array</span>
+        <ul>
+          {state?.sceneOne?.arrayOne?.map((entry, index) => (
+            <li key={index}>{entry}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
